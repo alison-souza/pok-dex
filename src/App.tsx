01c7f6fb1d96f9b24ceb73a302ad3fae07f5d7a2
alison-souza <alison.sxuzx@gmail.com>
2025-12-01 23:09:05 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
-import { Detail } from "./pages/PokemonDetails";
+import { PokemonDetails } from "./pages/PokemonDetails";
 import { FavoritesProvider } from "./context/FavoritesContext";
 
 export const App: React.FC = () => {
